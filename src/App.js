287a818bc3b0a572e579d33939e00b2aa656fa3d
Getaneh Tsegaye <getanehtsegaye@gmail.com>
2024-04-28 10:49:43 -0400

    Completed the Card component, used axios, fixed some routing bug, deleted app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import SideNav from "./components/sidenav/Sidenav";
-import Card from "./components/list/List";
-import Quotes from "./components/card/Card";
+import List from "./components/list/List";
+import Card from "./components/card/Card";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
 function App() {
   return (
-    // App Router
-
-    <BrowserRouter>
-      <div className="flex">
-        <SideNav />
-        <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-          <Routes>
-            <Route path="/" element={<Card />} />
-            <Route path="/card" element={<Card />} />
-            <Route path="/list" element={<Quotes />} />
-          </Routes>
+    <>
+      <BrowserRouter>
+        <div className="flex">
+          <SideNav />
+          <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+            <Routes>
+              <Route path="/" element={<Card />} />
+              <Route path="/card" element={<Card />} />
+              <Route path="/list" element={<List />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+import "./card.css";
 
-const Card = () => {
-  return <div>Card</div>;
-};
+function Card() {
+  const [quotes, setQuote] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://api.quotable.io/quotes/random?limit=6")
+      .then((respose) => {
+        // return respose.data;
+        console.log(respose.data);
+        setQuote(respose.data);
+      });
+  }, []);
+  return (
+    <>
+      <div>
+        <p className="cardHeader">
+          <span className="firstLetterBig">F</span>eeling unmotivated? We've got
+          just the cure: a healthy dose of questionable wisdom quotes of people
+          who are definitely, maybe smarter than your toaster. Explore our
+          collection quotes and see if you can find the inspiration (or
+          amusement) you need <b>Randomly Generated Quotes</b> and see if you
+          can find the inspiration (or amusement) you need.
+        </p>
+      </div>
+      <div className="cardGrid">
+        {quotes &&
+          quotes.map((quote, index) => (
+            <div className="cards" key={index}>
+              <img
+                className="card-image"
+                src={`https://picsum.photos/200/200?random=${index + 1}`}
+                alt="random image"
+              />
+              <h2 className="card-author">{quote.author}</h2>
+              <p className="card-quote">{quote.content}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+}
 
 export default Card;
