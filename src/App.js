22307c6a235453a20380ed2709a688003d36cfa2
Getaneh Tsegaye <getanehtsegaye@gmail.com>
2024-04-28 09:52:37 -0400

    Installed Tailwindcss and react router. Completed the side navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import SideNav from "./components/sidenav/Sidenav";
+import Card from "./components/list/List";
+import Quotes from "./components/card/Card";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "./App.css";
 function App() {
-  return <SideNav />;
+  return (
+    // App Router
+
+    <BrowserRouter>
+      <div className="flex">
+        <SideNav />
+        <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+          <Routes>
+            <Route path="/" element={<Card />} />
+            <Route path="/card" element={<Card />} />
+            <Route path="/list" element={<Quotes />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/sidenav/Sidenav.jsx
+++ src/components/sidenav/Sidenav.jsx
@@ -1,24 +1,70 @@
 import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 import Menus from "../../api/menu";
+import "./sidenav.css";
 
 const Sidenav = () => {
+  const [open, setOpen] = useState(true);
+  const location = useLocation();
+
   return (
-    <div>
-      <div>
-        <img src="/assets/control.png" />
-      </div>
-      <div>
-        <img src="/assets/logo.png" />
-        <h1>Quote of the Day</h1>
+    <div
+      id="sideNav"
+      className={`${
+        open ? "w-72" : "w-20"
+      } duration-300 h-screen p-5 pt-8 relative`}
+    >
+      <img
+        Class
+        src="/assets/control.png"
+        className={`absolute cursor-pointer rounded-full
+          -right-3 top-9 w-7 border-2 border-dark-purple ${
+            !open && "rotate-180"
+          }`}
+        onClick={() => setOpen(!open)}
+      />
+      <div className="flex gap-x-4 items-center">
+        <img
+          src="/assets/logo.png"
+          className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`}
+        />
+        <h1
+          className={`text-white origin-left font-medium text-xl duration-300 ${
+            !open && "scale-0"
+          }`}
+        >
+          {" "}
+          Quote of the Day
+        </h1>
       </div>
       <ul>
         {Menus.map((menu, index) => (
-          <li key={index}>
-            <img src={`../../assets/${menu.src}.png`} />
-            {menu.title}
+          <li
+            key={index}
+            className={`text-gray-300 text-sm cursor-pointer  hover:bg-light-white rounded-md
+           ${menu.gap ? "mt-9" : "mt-2"} ${
+              location.pathname === `/${menu.title.toLowerCase()}` &&
+              "bg-light-white"
+            }`}
+          >
+            <Link
+              className="flex items-center gap-x-4 p-2"
+              to={`/${menu.title.toLowerCase()}`}
+            >
+              <img src={`/assets/${menu.src}.png`} />
+              <span className={`${!open && "hidden"} origin-left duration-200`}>
+                {" "}
+                {menu.title}
+              </span>
+            </Link>
           </li>
         ))}
       </ul>
+      <div>
+        <ul></ul>
+      </div>
     </div>
   );
 };
